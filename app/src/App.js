96d3b3a75c10dd44763f7019d945df1f7134664a
akuzikov <akuzikov@sberhealth.ru>
2022-04-22 09:32:28 +0300

    build

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { TestingPage } from './pages/TestingPage'
 import { CabinetPage } from './pages/CabinetPage'
@@ -7,14 +7,14 @@ import { FirstTest } from './pages/TestingPage/FirstTest'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path='/cabinet' element={<CabinetPage />} />
         <Route path='/testing' element={<TestingPage />} />
         <Route path='/testing/1' element={<FirstTest />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
